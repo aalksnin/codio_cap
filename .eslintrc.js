ea03e52e2ffe246ce9baeb5e58302fa1d77a8640
Anton Alksnin <aalksnin@yahoo.com>
2025-07-18 03:50:11 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  globals: {
+    "$": true
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
